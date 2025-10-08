c232dbcea6928df07d1dc5a16abc0882c6bc73b9
Antonello Pagnozzi <antonello.pagnozzi@gmail.com>
2025-10-08 14:16:08 +0200

    First commit

---
+++ GameUp/src/main/java/com/jdk/GameUp/GameUpApplication.java
@@ -0,0 +1,13 @@
+package com.jdk.GameUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameUpApplication.class, args);
+	}
+
+}
---
+++ GameUp/src/test/java/com/jdk/GameUp/GameUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdk.GameUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
